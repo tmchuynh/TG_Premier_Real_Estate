1478cab5278f9ca67e1e318b761bd183ec47ee08
tmchuynh <tinamchuynh@gmail.com>
2023-06-18 20:05:32 -0700

    init with lorem ipsum and drawer menu from mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+// pages/_app.js
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// 1. Import `createTheme`
+import {NextUIProvider } from "@nextui-org/react"
+
+
+// 3. Pass the new `theme`` to the `NextUIProvider`
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider >
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
+
+export default MyApp
